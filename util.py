86712e69c0c224d1484fdb675ec29595d8c21ced
Gabriela Tavares <gtavares@caltech.edu>
2016-03-03 22:33:18 -0800

    Add sonnet data. Add utility functions for loading sonnets and writing sonnets to text file.

--- util.py
+++ util.py
@@ -5,7 +5,65 @@ util.py
 Authors: Gabriela Tavares,      gtavares@caltech.edu
          Juri Minxha,           jminxha@caltech.edu
 """
+import datetime
+import os
 
 
-def loadData():
-    return 0
+def loadShakespeareSonnets():
+    sonnets = []
+    with open('./shakespeare.txt', 'r') as f:
+        sonnet = []
+        sonnetToAppend = False
+        for line in f:
+            if line.strip().split(' ')[-1].isdigit():
+                sonnetToAppend = True
+                continue
+            if line == '\n':
+                if sonnetToAppend:
+                    sonnets.append(sonnet)
+                    sonnet = []
+                    sonnetToAppend = False
+                continue
+            sonnet.append(line.strip().split(' '))
+        sonnets.append(sonnet)
+    f.close()
+    return sonnets
+
+
+def loadSpenserSonnets():
+    sonnets = []
+    with open('./spenser.txt', 'r') as f:
+        sonnet = []
+        sonnetToAppend = False
+        skipLine = False
+        for line in f:
+            if line != '\n' and len(line.strip().split(' ')) == 1:
+                skipLine = True
+                sonnetToAppend = True
+                continue
+            if line == '\n':
+                if skipLine:
+                    skipLine = False
+                    continue
+                if sonnetToAppend:
+                    sonnets.append(sonnet)
+                    sonnet = []
+                    sonnetToAppend = False
+                continue
+            sonnet.append(line.strip().split(' '))
+        sonnets.append(sonnet)
+    f.close()
+    return sonnets
+
+
+def writeSonnetToTxt(sonnet):
+    if not os.path.isdir(os.getcwd() + '/sonnets/'):
+        os.mkdir(os.getcwd() + '/sonnets/')
+    fileName = os.getcwd() + '/sonnets/' + str(datetime.datetime.now())
+
+    with open(fileName, 'w') as f:
+        for line in sonnet:
+            for word in line:
+                f.write(word + ' ')
+            f.write('\n')
+    f.close()
